ce76939f04d865d4ec3523b10c6ce948f6ce0d47
Abvorobey <abvorobey@gmail.com>
2023-03-04 18:50:58 +0300

    Фибоначи

---
+++ Fibonachi/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+*/
+Console.Clear();
+int Fibonachi(int number)
+{
+    if(number ==1 || number == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return Fibonachi(number-1) + Fibonachi(number-2);
+    }
+}
+for (int i=1; i<11; i++)
+{
+    Console.WriteLine(Fibonachi(i));
+}
