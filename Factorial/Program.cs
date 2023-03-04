91d491e08639725a32cc646e5c4c28f93b6ddac9
Abvorobey <abvorobey@gmail.com>
2023-03-04 13:52:18 +0300

    Факториал рекурсией

---
+++ Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+*/
+Console.Clear();
+
+int GetFactorial (int number)
+{
+    if (number == 1)
+    {
+        return 1;
+    }
+    else 
+    {
+        return number*GetFactorial(number-1);
+    }
+
+}
+
+int result = GetFactorial (5);
+Console.WriteLine(result);
